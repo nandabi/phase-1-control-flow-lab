3a930694d6af368b480eb8f70238ad7113162f9d
nandabi <valerie.nandabi@student.moringaschool.com>
2023-12-19 22:10:57 +0300

    commit message

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x) {
   // Write your code here!
+  if (x <= 400) {
+    return "This one is on me!";
+  } else if (x > 2000 && x < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else return "No can do.";
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+return city == "NYC" ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+switch (tip){
+  case 'generous' : 
+return 'Thank you so much.';
+  case 'not as generous':
+return 'Thank you.'
+default : 'thanks for everything'
+return 'Bye.'
+}
 }
